9899a51864532e7c22b0733aad288c6ace90ac78
RoastSlav <raikov0411@gmail.com>
2022-02-14 00:57:05 +0200

    Getting the item from db to view
    Setup to get the items from the
    database to through the API and into
    the view

--- TRMDesktopUI.Library/Api/IAPIHelper.cs
+++ TRMDesktopUI.Library/Api/IAPIHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.Library.Api
@@ -6,6 +7,7 @@ namespace TRMDesktopUI.Library.Api
     public interface IAPIHelper
     {
         Task<AuthenticatedUser> Authenticate(string username, string password);
+        HttpClient ApiClient { get; }
 
         Task GetLoggedInUserInfo(string token);
     }
